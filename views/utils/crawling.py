2a02815af17e3b04b0eaf851d5f47e2b7c6459f2
JungboemLee <jungbum05@naver.com>
2022-12-04 21:17:17 +0900

    version 1.0.1

--- views/utils/crawling.py
+++ views/utils/crawling.py
@@ -3,10 +3,9 @@ from crawling_file.musinsa_crawling import MUSINSA_CRAWLING
 from crawling_file.googleplay_crawling import GOOGLEPLAY_CRAWLING
 import schedule
 
-schedule.every(1).minutes.do(MELON_CRAWLING.melon_crawling)
-schedule.every(1).minutes.do(MUSINSA_CRAWLING.musinsa_crawling)
-schedule.every(1).minutes.do(GOOGLEPLAY_CRAWLING.googleplay_crawling)
-
+schedule.every(1).hours.do(MELON_CRAWLING.melon_crawling)
+schedule.every(30).minutes.do(MUSINSA_CRAWLING.musinsa_crawling)
+schedule.every(1).hours.do(GOOGLEPLAY_CRAWLING.googleplay_crawling)
 
 while True :
     schedule.run_pending()
